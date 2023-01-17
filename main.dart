d777480165e245ced4aed1ac9f051baa428dc42f
Muhmmad Saad Ahmad <saadahmad63100@gmail.com>
2023-01-17 15:50:35 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:height_calculator/widgets/splash_screen.dart';
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "BMI Calculator",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home:  const SecondPage(),
+    );
+  }
+}
